24b0ec14f4f0b831ec0ddb941484347c6564d9ee
Alexandre Leroux <alex@sherpa.one>
2020-11-28 14:37:33 +0100

    refactoring

--- app.js
+++ app.js
@@ -3,19 +3,19 @@
 //await must be inside async
 (async () => {
 
-    const print = (something) => {
-        let ms = time_spent();
+    const print = (something, start) => {
+        let ms = time_spent(start);
         if (ms < 10) ms = `0${ms}`;
         console.log(`${ms}ms : ${something}`);
     }
 
-    const time_spent = () => {
+    const time_spent = (start) => {
         const do_now_date = Date.now();
-        return do_now_date - start_date;//milliseconds after runtime start
+        return do_now_date - start;//milliseconds after runtime start
     }
 
-    const do_now = (step_number) => {
-        print(`step ${step_number}`);
+    const do_now = (step_number, start) => {
+        print(`step ${step_number}`, start);
     }
 
     const do_after = (order, ms) => {
@@ -34,33 +34,33 @@
 
     const start_date = Date.now();
 
-    print("start");
+    print("start", start_date);
 
     do_after(1, 10).then(order => {
-        print(`step ${order}`)
+        print(`step ${order}`, start_date)
     }).catch(error => {
         console.error(error);
     })
 
     await do_after(2, 5).then(order => {
-        print(`step ${order}`)
+        print(`step ${order}`, start_date)
     }).catch(error => {
         console.error(error);
     })
 
-    do_now(3);
-    do_now(4);
-    do_now(5);
+    do_now(3, start_date);
+    do_now(4, start_date);
+    do_now(5, start_date);
 
-    print("end");
+    print("(theorical) end", start_date);
 
     /*
-    0ms : start
-    11ms : step 2
-    11ms : step 3
-    11ms : step 4
-    11ms : step 5
-    11ms : end
-    15ms : step 1
+    00ms : start
+    12ms : step 2
+    12ms : step 3
+    12ms : step 4
+    12ms : step 5
+    12ms : (theorical) end
+    16ms : step 1
     */
 })();
