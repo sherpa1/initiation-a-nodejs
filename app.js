dfa604e7d8864e7be30fa7cef69740b23ad5fa33
Alexandre Leroux <alex@sherpa.one>
2020-11-28 14:35:58 +0100

    refactoring

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
 'use strict';
 
-const print = (something) => {
-    const ms = what_time_is_it();
+const print = (something, start) => {
+    let ms = time_spent(start);
+    if (ms < 10) ms = `0${ms}`;
     console.log(`${ms}ms : ${something}`);
 }
 
-const what_time_is_it = () => {
-    const now_date = Date.now();
-    return now_date - start_date;//milliseconds after runtime start
+const time_spent = (start) => {
+    const do_now_date = Date.now();
+    return do_now_date - start;//milliseconds after runtime start
 }
 
-const now = (order) => {
-    print(`step ${order}`);
+const do_now = (step_number, start) => {
+    print(`step ${step_number}`, start);
 }
 
-const after = (order, ms) => {
+const do_after = (order, ms) => {
 
     return new Promise((resolve, reject) => {
         try {
@@ -28,34 +29,35 @@ const after = (order, ms) => {
 
 }
 
+
 const start_date = Date.now();
 
 
-print("start");
+print("start", start_date);
 
-after(1, 10).then(order => {
-    print(`step ${order}`)
+do_after(1, 10).then(order => {
+    print(`step ${order}`, start_date)
 }).catch(error => {
     console.error(error);
 })
 
-after(2, 5).then(order => {
-    print(`step ${order}`)
+do_after(2, 5).then(order => {
+    print(`step ${order}`, start_date)
 }).catch(error => {
     console.error(error);
 })
 
-now(3);
-now(4);
-now(5);
-print("end");
+do_now(3, start_date);
+do_now(4, start_date);
+do_now(5, start_date);
+print("(theorical) end", start_date);
 
 /*
-0ms : start
-5ms : step 3
-5ms : step 4
-5ms : step 5
-5ms : end
-10ms : step 2
-15ms : step 1
+00ms : start
+06ms : step 3
+06ms : step 4
+06ms : step 5
+06ms : (theorical) end
+12ms : step 2
+16ms : step 1
 */
